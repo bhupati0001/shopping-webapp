3234163b238eb0c977de9023005462541cfc02eb
bmohanta0001 <bmohanta0001@gmail.com>
2024-04-07 14:09:51 +0530

    the cart page is designed and the functionality of  total amount of the cart is also added

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './ProductDisplay.css'
 import star_icon from '../Assets/star_icon.png'
 import star_dull_icon from '../Assets/star_dull_icon.png'
+import { ShopContext } from '../../Context/ShopContext'
 
 const ProductDisplay = (props) => {
     const { product } = props;
+    const {addToCart} = useContext(ShopContext);
+
     return (
         <div className='ProductDisplay'>
             <div className="ProductDisplay-left">
@@ -50,7 +53,7 @@ const ProductDisplay = (props) => {
                         <div>XL</div>
                         <div>XXL</div>
                     </div>
-                    <button>Add to Cart</button>
+                    <button onClick={()=>{addToCart(product.id)}}>Add to Cart</button>
                     <p className="productDisplay-right-category"><span>Category:</span> Women, T-shirt, Crop Top</p>
                     <p className="productDisplay-right-category"><span>Tags:</span> Modern, Latest, Crop Top</p>
                     <p></p>
